4fbd7708683e7537f25fbc4cac0376534b0fcbd1
TheFigurehead <alex96813@gmail.com>
2017-04-17 15:33:00 +0300

    Positioning nav on main

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,21 @@
 
 var vid = document.getElementById("bgvid");
 
-if (window.matchMedia('(prefers-reduced-motion)').matches) {
-    vid.removeAttribute("autoplay");
-    vid.pause();
-    pauseButton.innerHTML = "Paused";
-}
-
 vid.addEventListener('ended', function()
 {
 vid.pause();
 });
+
+var interval = setInterval(checkTime, 1000);
+  function checkTime(){
+  if(Math.floor(vid.currentTime) > 7){
+    // var word = document.getElementById("words");
+    // if (word.style.display === 'none') {
+    //     word.style.display = 'block';
+    // } else {
+    //     word.style.display = 'none';
+    // }
+    alert("7 second");
+    clearInterval(interval);
+  }
+}
